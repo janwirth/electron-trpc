d231e8318680c4536b91e5336aad0f783ceea6fe
Jan Wirth <jottenwe@gmail.com>
2025-04-30 23:41:38 -0700

    Upgrade packages :boom:

--- examples/basic-react/src/index.tsx
+++ examples/basic-react/src/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import { ipcLink } from 'electron-trpc/renderer';
 import { createTRPCReact } from '@trpc/react-query';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -38,5 +38,6 @@ function HelloElectron() {
 
   return <div data-testid="greeting">{data.text}</div>;
 }
+const root = ReactDom.createRoot(document.getElementById('react-root')!);
 
-ReactDom.render(<App />, document.getElementById('react-root'));
+root.render(<App />);
